a2a51edc2137b24c9d62a4e4900d3541d0553a3f
cs19850402-wq <cs19850402@gmail.com>
2026-06-18 02:48:04 +0800

    Refactor get_trending function and update home route

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 from flask import Flask
-from collectors.github_collector import get_trending
+import requests
 
 app = Flask(__name__)
+
+def get_trending():
+    url = "https://api.github.com/search/repositories?q=stars:>1000&sort=stars&order=desc"
+
+    headers = {
+        "Accept": "application/vnd.github+json"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+
+    return {"items": []}
 
 @app.route("/")
 def home():
 
     data = get_trending()
 
-    result = []
+    result = [
+        "<h2>GitHub 熱門專案 TOP 10</h2>"
+    ]
 
     for repo in data["items"][:10]:
         result.append(
-            f"{repo['full_name']} ⭐{repo['stargazers_count']}"
+            f"""
+            <b>{repo['full_name']}</b><br>
+            ⭐ Stars: {repo['stargazers_count']}<br>
+            🔗 <a href="{repo['html_url']}">{repo['html_url']}</a>
+            <hr>
+            """
         )
 
-    return "<br>".join(result)
+    return "".join(result)
 
 if __name__ == "__main__":
-
-   s app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080)
